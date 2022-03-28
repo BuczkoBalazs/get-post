ba5558423dcdc1fd593a0f37d41f1484d296abc9
BuczkoBalazs <buci85@gmail.com>
2022-03-28 14:05:43 +0200

    jesus

--- frontend/pub/script.js
+++ frontend/pub/script.js
@@ -1,5 +1,23 @@
-fetch(".kismacska/data.json")
-    .then(response => response.json())
-    .then(data => {
-        
-    })
+const cardList = (title, sub, text) => {
+    return `
+    <div class="card">
+        <h3>${title}</h3>
+        <h4>${sub}</h4>
+        <h5>${text}</h5>
+    `
+}
+
+window.addEventListener("load", async () => {
+    let jsonData = await fetch("./kismacska/data.json");
+    let parsedData = await jsonData.json();
+
+    let cardData = parsedData.cards;
+
+    let result = "";
+
+    cardData.map(data => {
+        result += cardList(data.title,data.sub,data.text)
+    });
+
+    document.getElementById("root").insertAdjacentHTML("beforeend", result)
+})
